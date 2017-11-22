95704ca88b5da3e5d62cc57901cb5e8a9d7457b5
Elisa Trejo <etrejo23@gmail.com>
2017-11-22 04:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ let newID = 1000000000 + oldID;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.floor(Math.random() * 20);
 let randomInteger = Math.round(randomNumber);
+let randomUserID = randomInteger + oldID;
